0535a828c5f91cf42f4b00974d92de216f240e30
Mervan Yalçın <mervannyalcinn@gmail.com>
2023-07-27 01:44:35 +0300

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import "./globals.css";
+import { Inter, Nunito } from "next/font/google";
+
+import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./components/providers/ToasterProvider";
+
+
+export const metadata = {
+  title: "Airbnb",
+  description: "Airbnb Clone",
+};
+
+const font = Nunito({
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">f
+      <body className={font.className}>
+        <ClientOnly>
+          <ToasterProvider />
+          <RegisterModal />
+          <Navbar />
+        </ClientOnly>
+        {children}
+      </body>
+    </html>
+  );
+}
